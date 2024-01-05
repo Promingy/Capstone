b782e5544df27e4af6a048cbd495e0125eef3756
Corbin <140107765+Promingy@users.noreply.github.com>
2024-01-04 18:07:05 -0700

    npm run build

--- react-vite/src/components/MainPage/MainPage.jsx
+++ react-vite/src/components/MainPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import './MainPage.css'
-import { thunkDeleteRecipe, thunkGetAllRecipes, thunkUpdateRecipe } from '../../redux/recipe'
+import { thunkCreateRecipe, thunkDeleteRecipe, thunkGetAllRecipes, thunkUpdateRecipe } from '../../redux/recipe'
 import RecipeTile from './RecipeTile'
 
 export default function MainPage() {
@@ -12,14 +12,24 @@ export default function MainPage() {
         dispatch(thunkGetAllRecipes())
     }, [dispatch])
 
-    function test (e) {
+    function createTestRecipe (e) {
         e.preventDefault()
-        dispatch(thunkUpdateRecipe(6))
+        dispatch(thunkCreateRecipe({}))
+    }
+    function deleteRecipe (e) {
+        e.preventDefault()
+        dispatch(thunkDeleteRecipe(7))
+    }
+    function updateRecipe (e) {
+        e.preventDefault()
+        dispatch(thunkUpdateRecipe(8))
     }
 
     return (
         <>
-        <button onClick={test} >test</button>
+        <button onClick={createTestRecipe} >Create Recipe</button>
+        <button onClick={updateRecipe} >Update Recipe</button>
+        <button onClick={deleteRecipe} >Delete Recipe</button>
             {recipes && Object.keys(recipes).map(category => {
                 const categoryRecipes = Object.values(recipes[category])
 
