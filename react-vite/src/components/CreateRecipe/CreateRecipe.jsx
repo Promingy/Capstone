abe293185e47ef026c876778dfaa8530d8750085
Corbin <140107765+Promingy@users.noreply.github.com>
2024-01-05 17:53:04 -0700

    Significant form progress

--- react-vite/src/components/CreateRecipe/CreateRecipe.jsx
+++ react-vite/src/components/CreateRecipe/CreateRecipe.jsx
@@ -3,6 +3,7 @@ import "./CreateRecipe.css"
 import { useDispatch, useSelector } from 'react-redux'
 import { thunkGetDropdowns } from "../../redux/dropdown"
 import { useNavigate } from "react-router-dom"
+import TextareaAutoSize from 'react-textarea-autosize'
 
 export default function CreateRecipe () {
     const dispatch = useDispatch()
@@ -16,13 +17,19 @@ export default function CreateRecipe () {
     const [quantity, setQuantity] = useState(0)
     const [ingredients, setIngredients] = useState({})
     const [title, setTitle] = useState('')
-
+    const [description, setDescription] = useState('')
+    const [servings, setServings] = useState(0)
+    const [step, setStep] = useState('')
+    const [steps, setSteps] = useState({})
+    const [stepNumber, setStepNumber] = useState(1)
 
     useEffect(() => {
         dispatch(thunkGetDropdowns())
     }, [dispatch])
 
-    console.log('ingredients', ingredients)
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
 
     if (!sessionUser) navigate('/')
     if (!measurements || !categories) return
@@ -37,6 +44,28 @@ export default function CreateRecipe () {
                         placeholder="Title"
                         value={title}
                         onChange={ e => setTitle(e.target.value)}
+                    />
+                </label>
+
+                <label>
+                    <div className="description_container">
+                        <TextareaAutoSize
+                        className='create_description'
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                        />
+                        <span> {description.length} / 2000</span>
+                    </div>
+                </label>
+
+                <label>
+                    <input
+                        type='number'
+                        placeholder="Servings"
+                        min={1}
+                        value={servings || ''}
+                        onChange={e => setServings(e.target.value)}
                     />
                 </label>
 
@@ -57,23 +86,22 @@ export default function CreateRecipe () {
                     <div className="added_ingredients">
                         {Object.values(ingredients).map(ingredient => {
                             return (
-                                <p key={`ingredient${ingredients.ingredient}${ingredients.quantity}`}>
+                                <div key={`ingredient${ingredients.ingredient}${ingredients.quantity}`}>
                                     {ingredient.ingredient}
                                     <div className="remove_ingredient" onClick={() => {
-                                                const newIngredient = {...ingredients}
-                                                delete newIngredient[ingredient.ingredient]
-                                                setIngredients(newIngredient)
-
+                                            const newIngredient = {...ingredients}
+                                            delete newIngredient[ingredient.ingredient]
+                                            setIngredients(newIngredient)
                                         }}>
                                         <i className="fa-solid fa-x fa-xs" />
                                     </div>
-                                </p>
+                                </div>
                             )
                         })}
 
                     </div>
 
-                <label className="add_ingredients">
+                <label className="add_ingredients_container">
                     <select
                         value={measurement}
                         onChange={e => setMeasurement(e.target.value)}
@@ -96,7 +124,9 @@ export default function CreateRecipe () {
 
                     <input
                         type='number'
-                        value={quantity}
+                        placeholder="Quantity"
+                        value={quantity || ''}
+                        min={1}
                         onChange={e => setQuantity(e.target.value)}
                     />
                     <div className='add_ingredient' onClick={() => {
@@ -106,16 +136,119 @@ export default function CreateRecipe () {
                                                         quantity,
                                                         measurement}
                                 setIngredients({...ingredients, [ingredient]: newIngredient})
+
                                 // reset ingredient values
                                 setIngredient('')
                                 setQuantity(0)
                                 setMeasurement('Measurement')
                             }
                         }}>
-                        <i className="fa-solid fa-plus" />
+
+                        <div className="add_ingredient">
+                            <i className="fa-solid fa-plus" />
+                        </div>
 
                     </div>
                 </label>
+
+                <label>
+                    <div className="steps_container">
+                        <div className="added_steps">
+                            {Object.keys(steps).map(key => {
+                                const step = steps[key]
+                                return (
+                                <div key={`steps${step.stepNumber}`} className="steps_and_remove">
+                                    {step.stepNumber} {step.step}
+                                    <div className="remove_ingredient" onClick={() => {
+                                            const newSteps = {...steps}
+                                            delete newSteps[step.stepNumber]
+                                            setSteps(newSteps)
+                                        }}>
+                                        <i className="fa-solid fa-x fa-xs" />
+                                    </div>
+                                </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div className="step_and_add">
+                        <label>
+                            <input
+                                type="number"
+                                placeholder="Step"
+                                value={stepNumber}
+                                onChange={e => setStepNumber(e.target.value)}
+                                min={1}
+                                />
+                        </label>
+                        <div className="add_step_text_container">
+                            <TextareaAutoSize
+                                className="add_step_text_area"
+                                placeholder="Add Step"
+                                value={step}
+                                onChange={e => setStep(e.target.value)}
+                            />
+                            {step.length} / 2000
+                        </div>
+                        <div className='add_ingredient' onClick={() => {
+                            // add step to steps obj
+                            if (!step) return
+
+                            let newStepNum = stepNumber
+                            let test = 1
+
+                            while (steps[newStepNum - 1] == undefined && newStepNum !== 1){
+                                newStepNum -= 1
+                            }
+
+                            const newStep = {newStepNum, step}
+
+                            setSteps({...steps, [newStepNum]: newStep})
+
+                            // reset step values
+                            setStep('')
+                            setStepNumber(newStepNum + 1)
+                        }}>
+
+                        <div className="add_ingredient">
+                            <i className="fa-solid fa-plus" />
+                        </div>
+
+                    </div>
+
+                    </div>
+                </label>
+
+                <label>
+                        <span>Prep time:</span>
+                        <input
+                            type='number'
+                            placeholder="Hours"
+                            min={0}
+                            />
+                        <input
+                            type='number'
+                            placeholder="Minutes"
+                            min={0}
+                            max={60}
+                            />
+                </label>
+
+                <label>
+                        <span>Cook time:</span>
+                        <input
+                            type='number'
+                            placeholder="Hours"
+                            min={0}/>
+                        <input
+                            type='number'
+                            placeholder="Minutes"
+                            max={60}
+                            min={0}/>
+                </label>
+
+                <button>Submit</button>
             </form>
         </div>
     )
