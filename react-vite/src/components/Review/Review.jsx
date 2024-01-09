dbf9092ef0873da0d8d17445de111935f7d8b14a
Corbin <140107765+Promingy@users.noreply.github.com>
2024-01-08 17:53:56 -0700

    Almost finished left half of reviews

--- react-vite/src/components/Review/Review.jsx
+++ react-vite/src/components/Review/Review.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react'
 import './Review.css'
 
 export default function Review ({ recipe }) {
-    console.log(recipe)
+    const [rating, setRating] = useState(0)
+    const [hoverRating, setHoverRating] = useState(0)
+    const [ratingConfirmed, setRatingConfirmed] = useState(false)
+    function starCreatorHover() {
+        const stars = []
+
+        for (let i = 0; i < 5; i++){
+            stars.push(
+                <i
+                onMouseOver={() => setHoverRating(i + 1)}
+                onMouseLeave={() => {
+                    if (!ratingConfirmed) {
+                        setHoverRating(0)
+                    } else {
+                        setHoverRating(rating)
+                    }
+                }}
+                onClick={() => {
+                    setRatingConfirmed(true)
+                    setRating(i + 1)
+                }}
+                key={`${recipe.id}star${i}`}
+                className={`fa-solid fa-star fa-sm ${ i < hoverRating ? 'your_rating' : 'your_rating2'}`}/>
+            )
+        }
+
+        return stars
+    }
+
     return (
         <div className='review_container'>
             <div className='review_left'>
@@ -13,7 +42,13 @@ export default function Review ({ recipe }) {
                         <p className='reviews_out_of reviews_out_of_p'>{recipe.reviews.length} user ratings</p>
                     </div>
                 </div>
-                    <p>Your rating</p>
+                    <p className='your_rating_clear'>
+                        Your rating
+                        {!!rating && <span onClick={() => {setRating(0)}}>clear</span>}
+                    </p>
+                    <div className='your_rating_stars'>
+                        {starCreatorHover()}
+                    </div>
             </div>
             <div className='review_right'>
                 <h2 className='review_headers'>COOKING NOTES</h2>
