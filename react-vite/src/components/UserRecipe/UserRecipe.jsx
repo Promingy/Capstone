8294d0b7e3c726e65e47c4cf61b8ae2296acfb62
Corbin <140107765+Promingy@users.noreply.github.com>
2024-01-12 16:43:02 -0700

    styled header for user recipes

--- react-vite/src/components/UserRecipe/UserRecipe.jsx
+++ react-vite/src/components/UserRecipe/UserRecipe.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './UserRecipe.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { thunkGetUserRecipes } from '../../redux/session'
@@ -8,26 +8,42 @@ export default function UserRecipe() {
     const dispatch = useDispatch()
     const { userId } = useParams()
     let recipes = useSelector(state => state.session)
-    let recipeOwner;
+    const [recipeOwner, setRecipeOwner] = useState(null)
     recipes = recipes?.[userId]
 
-
-    console.log('recipes', recipes)
+    console.log('recipes', recipeOwner)
 
     useEffect(() => {
         dispatch(thunkGetUserRecipes(+userId))
     }, [dispatch])
 
+    function capFirstLeter(str) {
+        if (typeof str != 'string') return ''
+
+        const firstLetter = str[0].toUpperCase()
+        const body = str.slice(1).toLowerCase()
+
+        return firstLetter + body
+    }
+
     if (!recipes) return
 
     return (
         <div className='user_recipes_container'>
-            <header>
-                test
+            <header className='user_profile_container'>
+                <div className='header_left'>
+                    <h2>
+                        {capFirstLeter(recipeOwner?.first_name)} {capFirstLeter(recipeOwner?.last_name)}
+                    </h2>
+                    <img className='user_page_profile_pic' src={recipeOwner?.profile_pic}/>
+                </div>
+                <div className='header_right'>
+                    <p>{recipeOwner?.bio}</p>
+                </div>
             </header>
             <div>
                 {Object.values(recipes).map(recipe => {
-                    if (!recipeOwner) recipeOwner = recipe.owner
+                    if (!recipeOwner) return setRecipeOwner(recipe.owner)
 
                     return (
                         <div>
