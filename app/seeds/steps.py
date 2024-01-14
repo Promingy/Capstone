9b10029d9d6bda1ba09379e7864de83f238092d4
Corbin <140107765+Promingy@users.noreply.github.com>
2024-01-13 17:38:03 -0700

    updated seeds removed duplicates, fixed numbering prevening some seeds from seeding

--- app/seeds/steps.py
+++ app/seeds/steps.py
@@ -6,96 +6,6 @@ def seed_steps():
     Func that adds pre-seeded steps for the recipes to the db
     """
 
-    step1 = Step(
-    recipe_id=1,
-    step_number=1,
-    description="Boil water and cook spaghetti according to package instructions."
-    )
-
-    step2 = Step(
-        recipe_id=1,
-        step_number=2,
-        description="In a separate pan, brown ground beef until fully cooked."
-    )
-
-    step3 = Step(
-        recipe_id=1,
-        step_number=3,
-        description="Add tomato sauce to the cooked ground beef and simmer for 10 minutes."
-    )
-
-    step4 = Step(
-        recipe_id=2,
-        step_number=1,
-        description="Grill chicken breast until fully cooked."
-    )
-
-    step5 = Step(
-        recipe_id=2,
-        step_number=2,
-        description="Chop lettuce and slice cherry tomatoes for the salad."
-    )
-
-    step6 = Step(
-        recipe_id=2,
-        step_number=3,
-        description="Combine grilled chicken, lettuce, and cherry tomatoes in a large bowl."
-    )
-
-    step7 = Step(
-        recipe_id=3,
-        step_number=1,
-        description="Cut broccoli into florets and peel and slice carrots."
-    )
-
-    step8 = Step(
-        recipe_id=3,
-        step_number=2,
-        description="Stir-fry broccoli and carrots in a pan with soy sauce for 5 minutes."
-    )
-
-    step9 = Step(
-        recipe_id=3,
-        step_number=3,
-        description="Continue cooking until vegetables are tender."
-    )
-
-    step10 = Step(
-        recipe_id=4,
-        step_number=1,
-        description="Preheat oven to 350°F (175°C)."
-    )
-
-    step11 = Step(
-        recipe_id=4,
-        step_number=2,
-        description="In a bowl, cream together butter and sugar. Add eggs and vanilla; mix well."
-    )
-
-    step12 = Step(
-        recipe_id=4,
-        step_number=3,
-        description="In a separate bowl, combine flour, baking soda, and salt. Gradually add to the wet mixture."
-    )
-
-    step13 = Step(
-        recipe_id=5,
-        step_number=1,
-        description="Cook penne pasta according to package instructions. Drain and set aside."
-    )
-
-    step14 = Step(
-        recipe_id=5,
-        step_number=2,
-        description="Dice tomatoes and chop fresh basil."
-    )
-
-    step15 = Step(
-        recipe_id=5,
-        step_number=3,
-        description="Mix cooked pasta with tomatoes and fresh basil. Serve warm."
-    )
-    
     step16 = Step(
         recipe_id=1,
         step_number=1,
@@ -360,151 +270,151 @@ def seed_steps():
         description="In a bowl, assemble the burrito bowl with black beans, rice, tomatoes, and your favorite toppings."
     )
 
-    step60 = Step(
+    step133 = Step(
         recipe_id=16,
         step_number=1,
         description="Preheat the oven to 375°F."
     )
 
-    step61 = Step(
+    step134 = Step(
         recipe_id=16,
         step_number=2,
         description="In a mixing bowl, combine cream cheese, mayonnaise, and sour cream."
     )
 
-    step62 = Step(
+    step135 = Step(
         recipe_id=16,
         step_number=3,
         description="Add parmesan cheese and artichoke hearts, mix well."
     )
 
-    step63 = Step(
+    step136 = Step(
         recipe_id=16,
         step_number=4,
         description="Transfer the mixture to a baking dish."
     )
 
-    step64 = Step(
+    step137 = Step(
         recipe_id=16,
         step_number=5,
         description="Bake for 25 minutes or until the top is golden brown."
     )
 
-    step65 = Step(
+    step138 = Step(
         recipe_id=17,
         step_number=1,
         description="Cook fettuccine pasta according to package instructions."
     )
 
-    step66 = Step(
+    step139 = Step(
         recipe_id=17,
         step_number=2,
         description="In a pan, cook chicken breasts in butter until fully cooked."
     )
 
-    step67 = Step(
+    step140 = Step(
         recipe_id=17,
         step_number=3,
         description="Slice cooked chicken."
     )
 
-    step68 = Step(
+    step141 = Step(
         recipe_id=17,
         step_number=4,
         description="In the same pan, add heavy cream, parmesan cheese, and cooked fettuccine."
     )
 
-    step69 = Step(
+    step142 = Step(
         recipe_id=17,
         step_number=5,
         description="Stir until the sauce thickens and coats the pasta. Serve hot."
     )
 
-    step70 = Step(
+    step143 = Step(
         recipe_id=18,
         step_number=1,
         description="In a saucepan, heat heavy cream until it simmers."
     )
 
-    step71 = Step(
+    step144 = Step(
         recipe_id=18,
         step_number=2,
         description="Add semi-sweet chocolate and stir until melted."
     )
 
-    step72 = Step(
+    step145 = Step(
         recipe_id=18,
         step_number=3,
         description="In a separate bowl, whisk together egg yolks and granulated sugar."
     )
 
-    step73 = Step(
+    step146 = Step(
         recipe_id=18,
         step_number=4,
         description="Gradually whisk the chocolate mixture into the egg yolk mixture."
     )
 
-    step74 = Step(
+    step147 = Step(
         recipe_id=18,
         step_number=5,
         description="Stir in vanilla extract and pour the mixture into serving glasses. Chill before serving."
     )
 
-    step75 = Step(
+    step148 = Step(
         recipe_id=19,
         step_number=1,
         description="Place chopped romaine lettuce in a large salad bowl."
     )
 
-    step76 = Step(
+    step149 = Step(
         recipe_id=19,
         step_number=2,
         description="Add croutons, grated parmesan cheese, and caesar dressing."
     )
 
-    step77 = Step(
+    step150 = Step(
         recipe_id=19,
         step_number=3,
         description="Toss the salad until well coated with dressing."
     )
 
-    step78 = Step(
+    step151 = Step(
         recipe_id=19,
         step_number=4,
         description="Serve the salad on individual plates, garnished with black pepper."
     )
 
-    step79 = Step(
+    step152 = Step(
         recipe_id=19,
         step_number=5,
         description="Enjoy the classic Caesar salad!"
     )
 
-    step80 = Step(
+    step153 = Step(
         recipe_id=20,
         step_number=1,
         description="In a large pot, sauté chopped onion and minced garlic until fragrant."
     )
 
-    step81 = Step(
+    step154 = Step(
         recipe_id=20,
         step_number=2,
         description="Add chopped carrots and celery, cook until vegetables are tender."
     )
 
-    step82 = Step(
+    step155 = Step(
         recipe_id=20,
         step_number=3,
         description="Pour in canned crushed tomatoes and stir to combine."
     )
 
-    step83 = Step(
+    step156 = Step(
         recipe_id=20,
         step_number=4,
         description="Simmer the soup for 25 minutes, allowing flavors to meld."
     )
 
-    step84 = Step(
+    step157 = Step(
         recipe_id=20,
         step_number=5,
         description="Serve the tomato basil soup hot, garnished with your favorite herbs."
@@ -949,9 +859,6 @@ def seed_steps():
     )
 
     steps = [
-        step1, step2, step3, step4, step5,
-        step6, step7, step8, step9, step10,
-        step11, step12, step13, step14, step15,
         step16, step17, step18, step19, step20,
         step21, step22, step23, step24, step25,
         step26, step27, step28, step29, step30,
@@ -961,11 +868,6 @@ def seed_steps():
         step46, step47, step48, step49, step50,
         step51, step52, step53, step54, step55,
         step56, step57, step58, step59, step60,
-        step61, step62, step63, step64, step65,
-        step66, step67, step68, step69, step70,
-        step71, step72, step73, step74, step75,
-        step76, step77, step78, step79, step80,
-        step81, step82, step83, step84, step60,
         step61, step62, step63, step64, step65,
         step66, step67, step68, step69, step70,
         step71, step72, step73, step74, step75,
@@ -980,7 +882,12 @@ def seed_steps():
         step116, step117, step118, step119, step120,
         step121, step122, step123, step124, step125,
         step126, step127, step128, step129, step130,
-        step131, step132
+        step131, step132, step133, step134, step135,
+        step136, step137, step138, step139, step140,
+        step141, step142, step143, step144, step145,
+        step146, step147, step148, step149, step150,
+        step151, step152, step153, step154, step155,
+        step156, step157
     ]
 
     [db.session.add(step) for step in steps]
